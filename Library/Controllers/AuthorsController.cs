54631f83defabc0af76188ad6afd8ce0edec731e
jessicahattig <jessicahattig@gmail.com>
2023-11-01 19:43:13 -0700

    WIP: Controllers.

--- Library/Controllers/AuthorsController.cs
+++ Library/Controllers/AuthorsController.cs
@@ -10,7 +10,116 @@ namespace Library.Controllers
 {
     public class AuthorsController : Controller
     {
+    private readonly LibraryContext _db;
+    private readonly UserManager<ApplicationUser> _userManager;
 
+    public AuthorsController(UserManager<ApplicationUser> userManager, LibraryContext db)
+    {
+        _userManager = userManager;
+        _db = db;
     }
 
+    public async Task<ActionResult> Index()
+    {
+        string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);List<Author> userAuthors = _db.Authors
+                        .Where(entry => entry.User.Id == currentUser.Id)
+                        .Include(author => author.Book)
+                        .ToList();
+        return View(userAuthors);
+    }
+
+    public ActionResult Create()
+    {
+        ViewBag.AuthorId = new SelectList(_db.Books, "BookId", "Name");
+        return View();
+    }
+
+    [HttpPost]
+    public async Task<ActionResult> Create(Author author, int BookId)
+    {
+        if (!ModelState.IsValid)
+        {
+        ViewBag.BookId = new SelectList(_db.Books, "BookId", "Name");
+        return View(author);
+        }
+        else
+        {
+        string userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        ApplicationUser currentUser = await _userManager.FindByIdAsync(userId);
+        author.User = currentUser;
+        _db.Authors.Add(author);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+        }
+    }
+
+    public ActionResult Details(int id)
+    {
+        Author thisAuthor = _db.Authors
+        .Include(author => author.Book)
+        .Include(author => author.JoinEntities)
+        // .ThenInclude(join => join.Tag)
+        .FirstOrDefault(author => author.AuthorId == id);
+        return View(thisAuthor);
+    }
+
+    public ActionResult Edit(int id)
+    {
+        Author thisAuthor = _db.Authors.FirstOrDefault(author => author.AuthorId == id);ViewBag.BookId = new SelectList(_db.Books, "BooksId", "Name");
+        return View(thisAuthor);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Author author)
+    {
+        _db.Authors.Update(author);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+        Author thisAuthor= _db.Authors.FirstOrDefault(author => author.AuthorId == id);
+        return View(thisAuthor);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+        Author thisAuthor = _db.Authors.FirstOrDefault(author => author.AuthorId == id);_db.Authors.Remove(thisAuthor);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+    // public ActionResult AddTag(int id)
+    // {
+    //   Item thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+    //   ViewBag.TagId = new SelectList(_db.Tags, "TagId", "Title");
+    //   return View(thisItem);
+    // }
+
+    // [HttpPost]
+    // public ActionResult AddTag(Item item, int tagId)
+    // {
+    //   #nullable enable
+    //   ItemTag? joinEntity = _db.ItemTags.FirstOrDefault(join => (join.TagId == tagId && join.ItemId == item.ItemId));
+    //   #nullable disable
+    //   if (joinEntity == null && tagId != 0)
+    //   {
+    //     _db.ItemTags.Add(new ItemTag() { TagId = tagId, ItemId = item.ItemId });
+    //     _db.SaveChanges();
+    //   }
+    //   return RedirectToAction("Details", new { id = item.ItemId });
+    // }   
+
+    // [HttpPost]
+    // public ActionResult DeleteJoin(int joinId)
+    // {
+    //   ItemTag joinEntry = _db.ItemTags.FirstOrDefault(entry => entry.ItemTagId == joinId);
+    //   _db.ItemTags.Remove(joinEntry);
+    //   _db.SaveChanges();
+    //   return RedirectToAction("Index");
+    // } 
+    }
 }
--- Library/Controllers/PatronsController.cs
+++ Library/Controllers/PatronsController.cs
@@ -10,7 +10,69 @@ namespace Library.Controllers
 {
     public class PatronsController : Controller
     {
+    private readonly LibraryContext _db;
 
+    public PatronsController(LibraryContext db)
+    {
+        _db = db;
     }
 
+    public ActionResult Index()
+    {
+        List<Patron> model = _db.Patrons.ToList();
+        return View(model);
+    }
+
+    public ActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Patron patron)
+    {
+        _db.Patrons.Add(patron);
+        _db.SaveChanges();
+        return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+        Patron thisPatron = _db.Patrons
+                                .Include(pat => pat.Copies)
+                                .ThenInclude(patron => patron.JoinEntities)
+                                // .ThenInclude(join => join.Tag)
+                                .FirstOrDefault(patron => patron.CopyId == id);
+        return View(thisPatron);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      Patron thisPatron = _db.Patrons.FirstOrDefault(patron => patron.PatronId == id);
+      return View(thisPatron);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Patron patron)
+    {
+      _db.Patrons.Update(patron);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      Patron thisPatron = _db.Patrons.FirstOrDefault(patron => patron.PatronId == id);
+      return View(thisPatron);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      Patron thisPatron = _db.Patrons.FirstOrDefault(patron => patron.PatronId == id);
+      _db.Patrons.Remove(thisPatron);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
 }
